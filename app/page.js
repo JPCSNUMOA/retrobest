8d2439f49ebbdd5834d5a33550b1fb4e6eba1aa0
dwit729 <124338784+dwit729@users.noreply.github.com>
2024-12-02 21:51:28 +0800

    drafted frontend

--- app/page.js
+++ app/page.js
@@ -1,101 +1,46 @@
-import Image from "next/image";
+import Image from 'next/image'
+import gameboy from './assets/gameboy.png'
+import gameboy_site from './assets/gameboy_website.png'
+import lights from './assets/lights.png'
+import mainlogo from './assets/logomain.png'
 
-export default function Home() {
+
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className='overflow-auto lg:overflow-auto h-dvh lg:h-dvh w-dvw bg-black flex flex-col items-center xl:flex-row gap-5 lg:gap-0'>
+          <div className='xl:flex-1 flex flex-col justify-center lg:justify-around items items-center px-14 lg:pb-10 lg:h-5/6 h-auto'>
+            <div className='hidden xl:block h-8 w-5/6 items-center'>
+              <Image src={lights}/>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+            <div className='h-full lg:h-5/6'>
+              <Image src={mainlogo}/>
+            </div>
+
+            <p className='font-medium text-start w-5/6 hidden xl:block text-white drop-shadow-[0_0_5px_rgba(255,255,255,1)]'>JPCS NU MOA Chapter</p>
+
+          </div>
+          <div className='xl:flex-1 flex flex-col items-center h-auto justify-center xl:justify-between px-10 xl:px-5 xl:pt-16 gap-5'>
+              <p className='font-medium text-center w-full text-white drop-shadow-[0_0_5px_rgba(255,255,255,1)]'>Vote now for the Best Dressed and help us crown the ultimate retro vintage style icon!</p>
+              <div className='relative h-[598px] w-[560px] hidden xl:flex flex-col items-center justify-start'>
+                <Image className='absolute z-0' src={gameboy_site}/>
+                <p className='absolute top-[25%] text-2xl font-bold text-center text-[#ffa5dc] drop-shadow-[0_0_10px_rgba(255,102,196,1)] w-3/6'>BEST DRESSED</p>
+                <input placeholder='Enter Vote ID' className='text-white absolute top-[35%] bg-[#242424] p-2 rounded-lg border-2 w-3/6 mt-[] border-white z-10'/>
+                <input placeholder='Enter Candidate Name' className='text-white absolute top-[45%] bg-[#242424] p-2 rounded-lg border-2 w-3/6 mt-[] border-white z-10'/>
+                <button className='absolute top-[55%] text-white  bg-[#242424] px-1 rounded-lg border-2 w-[20%] mt-[] border-white z-10'>Submit !</button>
+              </div>
+              <div className='relative h-[545px]  w-[400px] flex xl:hidden flex-col items-center justify-start'>
+                <Image className='object-cover absolute z-0' src={gameboy}/>
+                <p className='absolute top-[15%] text-2xl font-bold text-center text-[#ffa5dc] drop-shadow-[0_0_10px_rgba(255,102,196,1)] w-3/6'>BEST DRESSED</p>
+                <input placeholder='Enter Vote ID' className='text-white absolute top-[22%] bg-[#242424] p-2 rounded-lg border-2 w-3/6 mt-[] border-white z-10'/>
+                <input placeholder='Enter Candidate Name' className='text-white absolute top-[32%] bg-[#242424] p-2 rounded-lg border-2 w-3/6 mt-[] border-white z-10'/>
+                <button className='absolute top-[42%] text-white  bg-[#242424] px-1 rounded-lg border-2 w-[20%] mt-[] border-white z-10'>Submit !</button>
+
+              </div>
+              <p className='font-medium text-center w-5/6 block xl:hidden text-white drop-shadow-[0_0_5px_rgba(255,255,255,1)]'>JPCS NU MOA Chapter</p>
+          </div>
+      </div>
+  )
 }
+
+export default page
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      dropShadow: {
+        'glow': '0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(255, 255, 255, 0.6)',
+      },
     },
   },
   plugins: [],
